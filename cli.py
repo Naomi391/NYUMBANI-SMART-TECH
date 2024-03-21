7035c41741c74a2a5dacb7bebb21fffcebf3f289
Naomi Mogi <nyabokenaomi001@gmail.com>
2024-03-21 15:05:35 +0300

    Tenants update

--- cli.py
+++ cli.py
@@ -155,7 +155,7 @@ def tenant_options():
         else:
             print("No rent payments found.")
 
-    elif user_input == '4':
+    elif user_input == '2':
         eviction_notices_data = get_eviction_notices_data()
         if eviction_notices_data:
             print("List of Eviction Notices:")
